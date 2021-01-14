257239c88f5ab863be0ce4054f7fa50d396c5536
Titrat Cristina <titratcristina@gmail.com>
2021-01-14 16:27:24 +0200

    Update footer.php

--- resurse/sectiuni/footer.php
+++ resurse/sectiuni/footer.php
@@ -1,4 +1,28 @@
-<footer class="container mt-5">
-    <p class="float-right"><a href="#">Back to top</a></p>
-    <p>&copy; 2017-2020 &middot; <a href="#">Legal</a> &middot; <a href="#">Termeni și condiții</a></p>
+<footer class="footer mt-5 pt-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-5 col-lg-5 col-xl-5">
+                <h5 class="mb-3">FESTINA LENTE</h5>
+                <p>Societatea FESTINA LENTE SRL operator feroviar cu capital privat, înfiinţată în anul 2014, are ca activitate principală transportul feroviar de călători.</p>
+                <p>&copy; 2020 - 2021 FESTINA LENTE S.R.L. &middot; Toate drepturile rezervate. Informațiile se pot schimba fără notificare, vă recomandăm să verificați site-ul înainte de călătoria dumneavoastră. Nu răspundem pentru informațiile provenite din surse neoficiale.</p>
+            </div>
+            <div class="col-12 col-md-3 col-lg-3 col-xl-3 float-right">
+                <h5 class="mb-3">Link-uri utile</h5>
+                <p><a href="#">Acasă</a></p>
+                <p><a href="#">Mersul trenurilor</a></p>
+                <p><a href="#">Anulări/Întârzieri</a></p>
+                <p><a href="#">Contact</a></p>
+                <p><a href="#">Detalii</a></p>
+            </div>
+            <div class="col-12 col-md-4 col-lg-4 col-xl-4">
+                <h5 class="mb-3">Contact</h5>
+                <p>București, strada. Şoseaua Pavel D. Kiseleff, sector 1, România</p>
+                <p>Telefon: 0123.456.789</p>
+                <p>Fax: 012.345.67.89</p>
+                <p>Email: contact@titrat.ro</p>
+                <p class="text-right"><a href="#">Back to top</a></p>
+            </div>
+
+        </div>
+    </div>
 </footer>
