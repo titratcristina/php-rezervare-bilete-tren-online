e934ff9413b983ad27a913ad53677b147088466e
Titrat Cristina <titratcristina@gmail.com>
2021-01-14 16:27:38 +0200

    Update meniu.php

--- resurse/sectiuni/meniu.php
+++ resurse/sectiuni/meniu.php
@@ -1,12 +1,11 @@
 <?php
-    session_start();
+session_start();
 ?>
 <nav class="navbar navbar-expand-md navbar-light fixed-top bg-white">
     <a class="navbar-brand" href="index.php">Festina lente</a>
     <!-- <a class="navbar-brand" href="#">Vade mecum</a> -->
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
-    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav mr-auto">
@@ -14,10 +13,10 @@
                 <a class="nav-link text-dark" href="index.php">Acasă</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link disabled" href="#">Mersul trenurilor</a>
+                <a class="nav-link text-dark" href="mersul-trenurilor.php">Mersul trenurilor</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link disabled" href="#">Anulări/Întârzieri</a>
+                <a class="nav-link text-dark" href="anulari.php">Anulări</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link text-dark" href="contact.php">Contact</a>
@@ -27,23 +26,28 @@
             </li>
         </ul>
 
-        <?php if(!isset($_SESSION['id'])): ?>
+        <?php if (!isset($_SESSION['id'])) : ?>
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link text-dark" href="inregistrare.php">Înregistrare</a>
+                    <a class="nav-link text-dark" href="inregistrare.php">Înregistrare sau Logare</a>
                 </li>
             </ul>
         <?php endif; ?>
-        
-        <?php if(isset($_SESSION['id'])): ?>
+
+        <?php if (isset($_SESSION['id'])) : ?>
             <ul class="navbar-nav ml-auto">
                 <li>
                     <span class="navbar-text">Salut, <?= $_SESSION['prenume'] ?></span>
                 </li>
-                <?php if($_SESSION['tip_utilizator']=="admin"): ?>
-                <li>
-                    <a class="nav-link text-dark" href="admin.php">Admin</a>
-                </li>
+                <?php if ($_SESSION['tip_utilizator'] == "normal") : ?>
+                    <li>
+                        <a class="nav-link text-dark" href="profil.php">Profil</a>
+                    </li>
+                <?php endif; ?>
+                <?php if ($_SESSION['tip_utilizator'] == "admin") : ?>
+                    <li>
+                        <a class="nav-link text-dark" href="admin.php">Admin</a>
+                    </li>
                 <?php endif; ?>
                 <li>
                     <a class="nav-link text-dark" href="autentificare.php?logout">Iesire</a>
