1f27a0824c7c32eaa5e5fa9ff4ef7d095cf96ea2
Titrat Cristina <titratcristina@gmail.com>
2021-01-14 16:26:42 +0200

    Update autentificare.php

--- autentificare.php
+++ autentificare.php
@@ -1,42 +1,43 @@
-<?php 
+<?php
 session_start();
 include 'db.php';
 
-if(isset($_POST['submit'])&&!isset($_SESSION['id'])){
-    $stmt = $conn->prepare("SELECT * FROM utilizatori WHERE email = ? AND parola = MD5(?)");
-    $stmt->bind_param("ss", $_POST['logareEmail'], $_POST['logareParola']);
+if (isset($_POST['submit']) && !isset($_SESSION['id'])) {
+	$stmt = $conn->prepare("SELECT * FROM utilizatori WHERE email = ? AND parola = MD5(?)");
+	$stmt->bind_param("ss", $_POST['logareEmail'], $_POST['logareParola']);
 
-    $stmt->execute();
+	$stmt->execute();
 
-    $user_details = $stmt->get_result();
+	$user_details = $stmt->get_result();
 
-    var_dump($stmt->num_rows);
+	var_dump($stmt->num_rows);
 
-    if($user_details->num_rows == 1){
-        $user_data = $user_details->fetch_assoc();
+	if ($user_details->num_rows == 1) {
+		$user_data = $user_details->fetch_assoc();
 
-        var_dump($user_data);
+		var_dump($user_data);
 
-        $_SESSION['id'] = $user_data['id'];
-        $_SESSION['prenume'] = $user_data['prenume'];
-        $_SESSION['tip_utilizator'] = $user_data['tip_utilizator'];
+		$_SESSION['id'] = $user_data['id'];
+		$_SESSION['nume'] = $user_data['nume'];
+		$_SESSION['prenume'] = $user_data['prenume'];
+		$_SESSION['email'] = $user_data['email'];
+		$_SESSION['data_nasterii'] = $user_data['data_nasterii'];
+		$_SESSION['tip_utilizator'] = $user_data['tip_utilizator'];
 
-        if($user_data['tip_utilizator']=="admin") header('Location: admin.php');
-        else header('Location: index.php');
-    } else {
-        header('Location: inregistrare.php?errorLogin');
-    }
+		if ($user_data['tip_utilizator'] == "admin") header('Location: admin.php');
+		else header('Location: index.php');
+	} else {
+		header('Location: inregistrare.php?errorLogin');
+	}
 
-    //   if($stmt->execute()) 
-    //     header('Location: ?success');
-    //   else 
-    //     header('Location: ?error');
+	//   if($stmt->execute()) 
+	//     header('Location: ?success');
+	//   else 
+	//     header('Location: ?error');
 
-    $stmt->close();
-} else if (isset($_GET['logout'])){
-    session_unset();
-    session_destroy();
-    header('Location: index.php');
+	$stmt->close();
+} else if (isset($_GET['logout'])) {
+	session_unset();
+	session_destroy();
+	header('Location: index.php');
 }
-
-?>
